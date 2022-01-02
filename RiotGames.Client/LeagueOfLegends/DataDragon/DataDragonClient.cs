8d7e35aa2698c8ca119312d8810eb660aa7f7a58
Mikael Dúi Bolinder <github@mikaelbolinder.se>
2022-01-02 16:23:32 +0100

    Create DataDragonClient.cs

---
+++ RiotGames.Client/LeagueOfLegends/DataDragon/DataDragonClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiotGames.LeagueOfLegends.DataDragon
+{
+    internal class DataDragonClient : IDisposable
+    {
+        private HttpClient _httpClient = new HttpClient();
+
+        public void Dispose() => _httpClient.Dispose();
+    }
+}

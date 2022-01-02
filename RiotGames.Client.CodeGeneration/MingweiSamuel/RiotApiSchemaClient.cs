776bad117a304baee8014222e8c872bc2270f42e
Mikael Dúi Bolinder <github@mikaelbolinder.se>
2022-01-02 16:31:48 +0100

    Removed pragma warning disable

--- RiotGames.Client.CodeGeneration/MingweiSamuel/RiotApiSchemaClient.cs
+++ RiotGames.Client.CodeGeneration/MingweiSamuel/RiotApiSchemaClient.cs
@@ -10,12 +10,10 @@ namespace MingweiSamuel
 {
     internal static class RiotApiSchemaClient
     {
-        public static async Task<RiotApiOpenApiSchema> GetOpenApiSchemaAsync(string version = "3.0.0")
+        public static async Task<RiotApiOpenApiSchema?> GetOpenApiSchemaAsync(string version = "3.0.0")
         {
             using (var client = new HttpClient())
-#pragma warning disable CS8603 // Possible null reference return.
                 return await client.GetFromJsonAsync<RiotApiOpenApiSchema>($"http://www.mingweisamuel.com/riotapi-schema/openapi-{version}.json");
-#pragma warning restore CS8603 // Possible null reference return.
         }
     }
 }

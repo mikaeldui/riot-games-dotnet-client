73304171d9454d81ef43f4af9af2091200d80af4
Mikael Dúi Bolinder <github@mikaelbolinder.se>
2022-01-03 02:01:04 +0100

    Using some cool indexer

--- RiotGames.Client.CodeGeneration/Extensions/HumanizerExtensions.cs
+++ RiotGames.Client.CodeGeneration/Extensions/HumanizerExtensions.cs
@@ -5,9 +5,9 @@ namespace RiotGames.Client.CodeGeneration
     public static class HumanizerExtensions
     {
         public static void PluralizeLast(this string[] source) =>
-            source[source.Length - 1] = source[source.Length - 1].Pluralize();
+            source[^1] = source[^1].Pluralize();
 
         public static void SingularizeLast(this string[] source) =>
-            source[source.Length - 1] = source[source.Length - 1].Singularize();
+            source[^1] = source[^1].Singularize();
     }
 }

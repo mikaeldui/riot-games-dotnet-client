e7e035aabdd881285f49f12518d10aa6380afae7
Mikael Dúi Bolinder <github@mikaelbolinder.se>
2022-01-05 17:46:23 +0100

    Fixed the CI for real

--- RiotGames.Client.CodeGeneration/Program.cs
+++ RiotGames.Client.CodeGeneration/Program.cs
@@ -9,7 +9,10 @@ if (!Debugger.IsAttached)
     // Let this still run on CI..
     await RiotGames.Client.CodeGeneration.RiotGamesApi.RiotApiRunner.GenerateCodeAsync();
 }
+else
+{
+    await RiotGames.Client.CodeGeneration.LeagueClient.LeagueClientRunner.GenerateCodeAsync();
+}
 
-await RiotGames.Client.CodeGeneration.LeagueClient.LeagueClientRunner.GenerateCodeAsync();
 
 Console.WriteLine("Program done!");

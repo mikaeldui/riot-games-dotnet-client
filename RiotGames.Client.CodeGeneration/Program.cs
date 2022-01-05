9140a8709281f3052dadff0c3b3f3b891b88f6f4
Mikael Dúi Bolinder <github@mikaelbolinder.se>
2022-01-05 13:41:28 +0100

    Fixed the CI

--- RiotGames.Client.CodeGeneration/Program.cs
+++ RiotGames.Client.CodeGeneration/Program.cs
@@ -1,8 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Diagnostics;
+
 Console.WriteLine("Welcome to the RiotGames.Client.CodeGeneration program!");
 
-//await RiotGames.Client.CodeGeneration.RiotGamesApi.RiotGamesApiClientsGenerator.GenerateCodeAsync();
+if (!Debugger.IsAttached)
+{
+    // Let this still run on CI..
+    await RiotGames.Client.CodeGeneration.RiotGamesApi.RiotGamesApiClientsGenerator.GenerateCodeAsync();
+}
 
 await RiotGames.Client.CodeGeneration.LeagueClient.LeagueClientApiClientGenerator.GenerateCodeAsync();
 

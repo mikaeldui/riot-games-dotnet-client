f1d7e178f3d45e93206d9e32dd8aaee9f4160c52
Mikael Dúi Bolinder <github@bolinder.uk>
2022-01-23 12:24:09 +0100

    Added support for custom Riot API base address.

--- RiotGames.Client/Http/RiotGamesApiHttpClient.cs
+++ RiotGames.Client/Http/RiotGamesApiHttpClient.cs
@@ -1,13 +1,27 @@
 ﻿using Camille.Enums;
+using System.ComponentModel;
 
 namespace RiotGames
 {
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public abstract class RiotGamesApiHttpClient
+    {
+        /// <summary>
+        /// Default it "https://{0}.api.riotgames.com" where {0} is replaced by the region, e.g. "euw1".
+        /// </summary>
+        public static string BaseAddressFormat { get; set; } = "https://{0}.api.riotgames.com";
+
+        private RiotGamesApiHttpClient()
+        {
+        }
+    }
+
     internal class RiotGamesApiHttpClient<TObjectBase> : RiotGamesHttpClient<TObjectBase>
     {
-        private RiotGamesApiHttpClient(string apiKey, string apiSubDomain)
+        private RiotGamesApiHttpClient(string apiKey, string route)
         {
             HttpClient.DefaultRequestHeaders.TryAddWithoutValidation("X-Riot-Token", apiKey);
-            BaseAddress = new Uri($"https://{apiSubDomain}.api.riotgames.com");
+            BaseAddress = new Uri(string.Format(RiotGamesApiHttpClient.BaseAddressFormat, route));
         }
 
         internal RiotGamesApiHttpClient(string apiKey, RegionalRoute region) : this(apiKey, region.ToString().ToLower())
